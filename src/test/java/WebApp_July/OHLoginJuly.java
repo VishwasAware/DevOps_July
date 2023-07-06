c24a1833eff64dc8715fc8310b9e308a2cec9ce7
VishwasAware <135203945+VishwasAware@users.noreply.github.com>
2023-07-06 20:17:41 +0530

    Headless browser steps added

--- src/test/java/WebApp_July/OHLoginJuly.java
+++ src/test/java/WebApp_July/OHLoginJuly.java
@@ -3,12 +3,15 @@ package WebApp_July;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+//import org.testng.annotations.Test;
 
 public class OHLoginJuly {
   @Test
   public void VerifyTitle() throws InterruptedException {
-	  
-	  WebDriver driver = new ChromeDriver();
+	  ChromeOptions chrome_Option = new ChromeOptions();
+	  chrome_Option.addArguments("--headless");
+	  WebDriver driver = new ChromeDriver(chrome_Option);
 	  driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index");
 	  String Title = driver.getTitle();
 	  System.out.println("Title is:- "+Title);
